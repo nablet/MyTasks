4f3a4c82c3ab19c1f77bb1b0c7d5fd8e2cb9a9ee
Jayson Tiongco <jaysonti@Macintosh.local>
2022-01-22 16:31:41 +0800

    Add experimentalmaterialapi annotation to mainactivity

--- androidApp/src/main/java/com/nablet/mytasks/android/MainActivity.kt
+++ androidApp/src/main/java/com/nablet/mytasks/android/MainActivity.kt
@@ -3,9 +3,11 @@ package com.nablet.mytasks.android
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.ExperimentalMaterialApi
 import com.nablet.mytasks.android.presentation.navigation.Navigation
 import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalMaterialApi
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
