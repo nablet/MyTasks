1e835fa56fd55e0aac75d10990b94aef4f96da3f
Jayson Tiongco <jaysonti@Macintosh.local>
2022-01-22 19:31:32 +0800

    Update android app version

--- buildSrc/src/main/kotlin/Application.kt
+++ buildSrc/src/main/kotlin/Application.kt
@@ -1,7 +1,7 @@
 object Application {
 	const val appId = "com.nablet.mytasks.android"
-	const val versionCode = 1
-	const val versionName = "1.0"
+	const val versionCode = 2
+	const val versionName = "1.1"
 	const val minSdk = 23
 	const val compileSdk = 32
 	const val targetSdk = 32
